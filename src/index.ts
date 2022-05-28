7b4b42b80600fc46d322521c75160d553c190bd4
Santo Pfingsten <core@lusito.info>
2022-05-28 13:32:43 +0200

    check link result

--- src/index.ts
+++ src/index.ts
@@ -40,42 +40,53 @@ export function createShaderProgram<TC extends WebGLRenderingContext, TD extends
     fragmentShaderSource: string,
     data: TD
 ): ShaderProgram<TD> {
-    const program = gl.createProgram() as WebGLProgram;
-    let vertexShader: WebGLShader;
-    let fragmentShader: WebGLShader;
+    let program = gl.createProgram();
+    if (!program) throw new Error("Could not create a new program");
+
+    let vertexShader: WebGLShader | undefined;
+    let fragmentShader: WebGLShader | undefined;
+
+    const dispose = () => {
+        if (program) {
+            gl.useProgram(null);
+            if (fragmentShader) gl.deleteShader(fragmentShader);
+            if (vertexShader) gl.deleteShader(vertexShader);
+            gl.deleteProgram(program);
+            program = null;
+        }
+    };
 
     try {
         vertexShader = addShader(gl, program, gl.VERTEX_SHADER, vertexShaderSource);
         fragmentShader = addShader(gl, program, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            const infoLog = gl.getProgramInfoLog(program);
+            throw new Error(`Failed linking program: ${infoLog ?? "unknown error"}`);
+        }
+        gl.useProgram(program);
+
+        const setters: any = {};
+        for (const key of Object.keys(data)) {
+            const fn = data[key];
+            const factory = factoriesGl2[fn];
+            if (!factory) throw new Error(`Can't handle ${fn}!`);
+            const location = fn.startsWith("uniform")
+                ? gl.getUniformLocation(program, key)
+                : gl.getAttribLocation(program, key);
+            setters[key] = factory(gl as unknown as WebGL2RenderingContext, location as any);
+        }
+
+        return {
+            ...setters,
+            use() {
+                gl.useProgram(program);
+            },
+            dispose,
+        };
     } catch (e) {
-        gl.deleteProgram(program);
+        dispose();
         throw e;
     }
-
-    gl.linkProgram(program);
-    gl.useProgram(program);
-
-    const setters: any = {};
-    for (const key of Object.keys(data)) {
-        const fn = data[key];
-        const factory = factoriesGl2[fn];
-        if (!factory) throw new Error(`Can't handle ${fn}!`);
-        const location = fn.startsWith("uniform")
-            ? gl.getUniformLocation(program, key)
-            : gl.getAttribLocation(program, key);
-        setters[key] = factory(gl as unknown as WebGL2RenderingContext, location as any);
-    }
-
-    return {
-        ...setters,
-        use() {
-            gl.useProgram(program);
-        },
-        dispose() {
-            gl.useProgram(null);
-            gl.deleteShader(fragmentShader);
-            gl.deleteShader(vertexShader);
-            gl.deleteProgram(program);
-        },
-    };
 }
